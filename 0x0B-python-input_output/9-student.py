781673f9ad1ec90256e59fa3b1f4fa36f7b2f2df
Sir-Blow <emmanuelakej@gmail.com>
2023-04-13 06:47:14 -0700

    Commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,13 +3,15 @@
 
 
 class Student:
-    """representation of a student by public instance attributes"""
+    """student representation"""
     def __init__(self, first_name, last_name, age):
-        """instantiation of the student"""
+        """instantiatiate the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """retrieves a dictionary representation of a Student instance"""
+        """
+        retrieves a dictionary representation of a Student instance
+        """
         return self.__dict__
