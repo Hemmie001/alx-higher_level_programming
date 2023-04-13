6eb3d220e4cc4046320396f96e95bcafddf7eb89
Sir-Blow <emmanuelakej@gmail.com>
2023-04-12 17:40:19 -0700

    commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def number_of_lines(filename="", text=""):
-    """ writes a string to a text file (UTF8) 
+    """ writes a string to a text file (UTF8)
     and returns the number of characters written"""
     with open(filename, mode="r", encoding="utf-8") as text_file:
         counter = 0
