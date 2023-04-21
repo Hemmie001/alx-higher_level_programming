6aa54fcc3c96253c35040e7a5e2cc61081aae6fc
Sir-Blow <emmanuelakej@gmail.com>
2023-04-21 02:09:29 -0700

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -68,12 +68,36 @@ class Base:
             new_inst.update(**dictionary)
         return new_inst
 
-    @classmethod
+        """
+        @classmethod
     def load_from_file(cls):
         """load a list of instancesfrom a file"""
         try:
             with open(str(cls.__name__) + ".json", "r") as jsonfile:
                 list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+               except IOError:
+            return []
+        """
+
+        @classmethod
+    def load_from_file_csv(cls):
+        """Return a list of classes instantiated from a CSV file.
+        Reads from `<cls.__name__>.csv`.
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
