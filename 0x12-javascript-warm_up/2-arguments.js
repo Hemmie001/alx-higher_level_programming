ca34d1ed7ba4d297dd5f41c7df05705e7a3ff41d
Sir-Blow <emmanuelakej@gmail.com>
2023-06-15 15:18:41 -0700

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-if (process.argv.length < 1)
+if (process.argv.length < 3)
 {
-	console.log("No argument");
+  console.log('No argument');
 }
-else if (process.argv.length === 1)
+else if (process.argv.length === 3)
 {
-	console.log("Argument found");
+  console.log('Argument found');
 }
 else 
 {
-	console.log("Arguments found"i);
+  console.log('Arguments found');
 }
