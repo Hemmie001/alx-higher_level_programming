442dad5b2863ae3c8ab18b1d163052448802b1ab
Sir-Blow <emmanuelakej@gmail.com>
2023-06-19 08:50:46 -0700

    0 correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,16 +13,16 @@ Your code should not be executed when imported
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == "__main__":  # makes code not to execute when imported
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()  # creates a cursur-lets you execute queries
-    cursor.execute("SELECT * FROM states")  # Use all the SQL u like
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
 
-    # print all the first cell of all the rows
     for data in cursor.fetchall():
         print(data)
     cursor.close()
-    db.close()i
+
+    db.close()
