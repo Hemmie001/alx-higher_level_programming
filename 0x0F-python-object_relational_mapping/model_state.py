b59e20d15704614adc6fd73d689ef57343c4f816
Sir-Blow <emmanuelakej@gmail.com>
2023-06-19 19:31:23 -0700

    6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
 """
-import sys
-from model_state import Base, State
+This is a python file that contains the class definition of a State
+and an instance Base = declarative_base():
 
-from sqlalchemy import (create_engine)
+State class:
+1 - inherits from Base Tips
+2 - links to the MySQL table states
+3 - class attribute id that represents a column of an auto-generated,
+****unique integer, can’t be null and is a primary key
+4 - class attribute name that represents a column of a string
+****with maximum 128 characters and can’t be null
+You must use the module SQLAlchemy
+This script connects to a MySQL server running on localhost at port 3306
+WARNING: all classes who inherit from Base must be imported before
+calling Base.metadata.create_all(engine)a python file that contains
+the class definition of a State and an instance Base = declarative_base():
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Representation of a State"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, )
+    name = Column(String(128), nullable=False)
