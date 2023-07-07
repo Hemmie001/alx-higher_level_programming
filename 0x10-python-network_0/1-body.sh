14e8ea669eb528d3d65115c7adb7c52309876705
Sir-Blow <emmanuelakej@gmail.com>
2023-07-07 14:31:59 -0700

    1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#this script takes in a URL,
-#sends a GET request to the URL, 
-#and displays the body of the response
-#It display only body of a 200 status code response
-#It was requested I make use of curl
+# This scripts 
 curl -sX GET $1 -L
