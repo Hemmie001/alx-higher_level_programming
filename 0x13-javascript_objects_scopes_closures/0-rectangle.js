c7e3dd632380d2a53e774a95fe58b2a988288024
Sir-Blow <emmanuelakej@gmail.com>
2023-07-08 05:26:52 -0700

    0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+//This js script writes an empty class Rectangle that defines a rectangle
+//The class notation is used for defining the class
+class Rectangle {}
+module.exports = Rectangle;
