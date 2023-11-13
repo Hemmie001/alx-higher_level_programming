98d8ffac5bd5cc5cfc249f8b9a385def2bc43976
Hemmie001 <emmanuelakej@gmail.com>
2023-11-13 12:54:41 -0800

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,23 +2,26 @@
 """ Find a peak """
 
 def find_peak(list_of_integers):
+
+
     """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
 
     size = len(list_of_integers)
     mid = int(length / 2)
+    loi = list_of_integers
 
     if mid - 1 < 0 and mid + 1 >= length:
-        return list_of_integers[mid]
+        return loi[mid]
     elif mid - 1 < 0:
-        return list_of_integers[mid] if list_of_integers[mid] > list_of_integers[mid + 1] else list_of_integers[mid + 1]
+        return loi[mid] if loi[mid] > loi[mid + 1] else loi[mid + 1]
     elif mid + 1 >= length:
-        return list_of_integers[mid] if list_of_integers[mid] > list_of_integers[mid - 1] else list_of_integers[mid - 1]
+        return loi[mid] if loi[mid] > loi[mid - 1] else loi[mid - 1]
 
-    if list_of_integers[mid - 1] < list_of_integers[mid] > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
+    if loi[mid - 1] < loi[mid] > loi[mid + 1]:
+        return loi[mid]
 
-    if list_of_integers[mid + 1] > list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[mid:])
-    return find_peak(list_of_integers[:mid]
+    if loi[mid + 1] > loi[mid - 1]:
+        return find_peak(loi[mid:])
+    return find_peak(loi[:mid]
