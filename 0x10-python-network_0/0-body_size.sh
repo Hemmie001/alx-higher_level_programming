92405dc15cdaea3caa169a9ce2a38bdbb96c1ea6
Hemmie001 <emmanuelakej@gmail.com>
2023-11-14 04:45:16 -0800

    try

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-#This script displays the size of the body of the response of a request
-#curl -s -I "$1" | grep Content-Length | awk '{print $2}'
-curl_status=$(curl -s -o /dev/null -w '%{http_code}' --connect-timeout 3 http://0.0.0.0:5000/twi?c0=24+91+24+91+24+91+24+91)``
+
