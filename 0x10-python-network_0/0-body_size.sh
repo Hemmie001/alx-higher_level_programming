fb4510ee299e1e0aee5be09b76abce50a3cb0f4b
Hemmie001 <emmanuelakej@gmail.com>
2023-11-14 05:11:33 -0800

    k

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+# displays only the status code of the response.
+#curl -so /dev/null -w "%{http_code}" "$1"
+#curl --write-out "%{http_code}" --silent --output /dev/null "$1"
+curl --write-out %{http_code} --silent --output /dev/null ${URL}
